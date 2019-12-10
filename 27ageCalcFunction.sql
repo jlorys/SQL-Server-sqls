bccae6650c4816e4a27fd9844a2fed8a55e8e602
Kuba <kuba35@vp.pl>
2019-12-10 15:00:07 +0100

    simplify function calculating age

--- 27ageCalcFunction.sql
+++ 27ageCalcFunction.sql
@@ -1,24 +1,17 @@
-CREATE FUNCTION fnComputeAge(@DOB DATETIME)
-RETURNS NVARCHAR(50)
-AS
-BEGIN
+DECLARE @DOB DATETIME
+SET @DOB = '1993-07-11 19:45:31.793'
 
 DECLARE @tempdate DATETIME, @years INT, @months INT, @days INT
 SELECT @tempdate = @DOB
 
-SELECT @years = DATEDIFF(YEAR, @tempdate, GETDATE()) - CASE WHEN (MONTH(@DOB) > MONTH(GETDATE())) OR (MONTH(@DOB) = MONTH(GETDATE()) AND DAY(@DOB) > DAY(GETDATE())) THEN 1 ELSE 0 END
+SELECT @years = DATEDIFF(YEAR, @tempdate, GETDATE()) - IIF((MONTH(@DOB) >= MONTH(GETDATE())) AND DAY(@DOB) > DAY(GETDATE()), 1, 0)
 SELECT @tempdate = DATEADD(YEAR, @years, @tempdate)
 
-SELECT @months = DATEDIFF(MONTH, @tempdate, GETDATE()) - CASE WHEN DAY(@DOB) > DAY(GETDATE()) THEN 1 ELSE 0 END
+SELECT @months = DATEDIFF(MONTH, @tempdate, GETDATE()) - IIF(DAY(@DOB) > DAY(GETDATE()), 1, 0)
 SELECT @tempdate = DATEADD(MONTH, @months, @tempdate)
 
 SELECT @days = DATEDIFF(DAY, @tempdate, GETDATE())
 
-DECLARE @Age NVARCHAR(50)
-SET @Age = Cast(@years AS  NVARCHAR(4)) + ' Years ' + Cast(@months AS  NVARCHAR(2))+ ' Months ' +  Cast(@days AS  NVARCHAR(2))+ ' Days Old'
-RETURN @Age
-
-End
-
---check
-Select fnComputeAge('1993-07-23 19:45:31.793')
+SELECT Cast(@years AS  NVARCHAR(4)) + ' Years ' + 
+       Cast(@months AS  NVARCHAR(2))+ ' Months ' +  
+       Cast(@days AS  NVARCHAR(2))+ ' Days Old'
