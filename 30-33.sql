b5310c781864bf6144e9bbb16e4de4d87ba5dd0c
Kuba <kuba35@vp.pl>
2019-12-15 11:34:41 +0100

    add schema binding option explanation

--- 30-33.sql
+++ 30-33.sql
@@ -18,5 +18,23 @@ Select * from fn_EmployeesByGender('Male')
 --1. Inline Table Valued functions can be used to achieve the functionality of parameterized views. We will talk about views, in a later session.
 --2. The table returned by the table valued function, can also be used in joins with other tables.
 
+--Deterministic and Nondeterministic Functions:
+--Deterministic functions always return the same result any time they are called with a specific set of input values and given the same state of the database. 
+--Examples: Sum(), AVG(), Square(), Power() and Count()
 
+--Note: All aggregate functions are deterministic functions.
+
+--Nondeterministic functions may return different results each time they are called with a specific set of input values even if the database state that they access remains the same.
+--Examples: GetDate() and CURRENT_TIMESTAMP
+
+--Rand() function is a Non-deterministic function, but if you provide the seed value, the function becomes deterministic, as the same value gets returned for the same seed value.
+
+--below schema binding prevents dropping source table
+Alter Function fn_GetEmployeeNameById(@Id int)
+Returns nvarchar(20)
+With SchemaBinding
+as
+Begin
+ Return (Select Name from dbo.tblEmployees Where Id = @Id)
+End
 
